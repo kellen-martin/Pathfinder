dffb4c67841d64122fed35f13cea97cacd096aae
Kellen Martin <kema0161@colorado.edu>
2025-04-20 17:44:17 -0500

    add new trajectory class to optimization

--- Final_Test.py
+++ Final_Test.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-import sys
-sys.path.append(r"C:\\Users\\18475\\Desktop\\Projects\\GMAT\\api")
-from load_gmat import gmat
 import openmdao.api as om
+from astropy.time import Time
+from astropy.coordinates import solar_system_ephemeris, get_body_barycentric_posvel
 
 #This is similar as IDF code structure
 
@@ -26,32 +25,83 @@ class Dry_mass(om.ExplicitComponent):
         outputs['m_d'] = m_hull+m_s
 
 #Trajectory Block
+# Define stumpff function for lambert solver
+def stumpff_c2(psi):
+    if psi > 1e-6:
+        return (1 - np.cos(np.sqrt(psi))) / psi
+    elif psi < -1e-6:
+        return (1 - np.cosh(np.sqrt(-psi))) / psi
+    else:
+        return 0.5
+
+def stumpff_c3(psi):
+    if psi > 1e-6:
+        return (np.sqrt(psi) - np.sin(np.sqrt(psi))) / (psi ** 1.5)
+    elif psi < -1e-6:
+        return (np.sinh(np.sqrt(-psi)) - np.sqrt(-psi)) / ((-psi) ** 1.5)
+    else:
+        return 1/6
+
+# Universal Lambert's Problem solver using bisection
+def UV_Lambert_Bisect(r1_vec, r2_vec, mu, delta_t, tol=1e-4, max_iter=1000):
+    r1 = np.linalg.norm(r1_vec)
+    r2 = np.linalg.norm(r2_vec)
+    c_theta = np.dot(r1_vec, r2_vec) / (r1 * r2)
+    A = np.sqrt(r1 * r2 * (1 + c_theta))
+
+    psi_low, psi_up = -4*np.pi**2, 4*np.pi**2
+    psi = 0
+    n = 0
+
+    while n < max_iter:
+        c2 = stumpff_c2(psi)
+        c3 = stumpff_c3(psi)
+        try:
+            y = r1 + r2 + (A * (psi * c3 - 1) / np.sqrt(c2))
+            chi = np.sqrt(y / c2)
+            dt_guess = (chi ** 3 * c3 + A * np.sqrt(y)) / np.sqrt(mu)
+        except:
+            psi += 1e-4
+            n += 1
+            continue
+
+        if abs(dt_guess - delta_t) < tol:
+            break
+
+        if dt_guess < delta_t:
+            psi_low = psi
+        else:
+            psi_up = psi
+
+        psi = 0.5 * (psi_low + psi_up)
+        n += 1
+
+    f = 1 - y / r1
+    g = A * np.sqrt(y / mu)
+    g_dot = 1 - y / r2
+
+    v1 = (r2_vec - f * r1_vec) / g
+    v2 = (g_dot * r2_vec - r1_vec) / g
+    return v1, v2, n
+
 class Trajectory(om.ExplicitComponent):
     """
-    This class is used to calculate the delta v required for each burn 
+    This class is used to calculate the total delta v required for the mission
     """
 
     def setup(self):
 
         # Time vector as an input
         # [EarthWait EMDeltaT MarsWait MEDeltaT]
-        self.add_input("EDB", val = np.zeros(3))
-        self.add_input("MAB", val=np.zeros(3))
-        self.add_input("MDB", val = np.zeros(3))
-        self.add_input("EAB", val=np.zeros(3))
         self.add_input("ts", val = np.zeros(3))
         self.add_input("t_start", val=0)
 
         # delta-v magnitudes  as an output
         self.add_output("delta_v", val = 0.0)
-        self.add_output("MarsRMAG")
-        self.add_output("MarsVX")
-        self.add_output("MarsVY")
-        self.add_output("MarsVZ")
-        self.add_output("EarthRMAG")
-        self.add_output("EarthVX")
-        self.add_output("EarthVY")
-        self.add_output("EarthVZ")
+        self.add_output("EDB", val=np.zeros(3))
+        self.add_output("MAB", val=np.zeros(3))
+        self.add_output("MDB", val=np.zeros(3))
+        self.add_output("EAB", val=np.zeros(3))
 
     def setup_partials(self):
         self.declare_partials('*', '*', method = 'fd')
@@ -63,88 +113,57 @@ class Trajectory(om.ExplicitComponent):
         """
         ts = inputs['ts']
         t_start = inputs['t_start']
-        EarthDepartureBurn = inputs['EDB']
-        MarsArrivalBurn = inputs['MAB']
-        MarsDepartureBurn = inputs['MDB']
-        EarthArrivalBurn = inputs['EAB']
         """
-        Call GMAT to calculate the delta-v vectors
-        delta-vs = [delta_v1, delta_v2, ..., delta_vn]
+        Calls Lambert's Problem Solver to find delta-v required for the given times
         """
+        mu_sun = 1.32715e11  # [km^3/s^2]
     
-        # Load the GMAT Script
-        gmat.LoadScript('C:\\Users\\18475\\Desktop\\Projects\\GMAT\\missions\\Pathfinder_EM.script')
         # Set Times in GMAT
-        start_date = 30770 # UTCMod Julian Date April 4, 2025
-        EarthWait = t_start + start_date
+        start_date = Time('2025-04-20', scale='tdb').jd  # Today's Julian date
+        EarthDepart = t_start + start_date
         EMDeltaT = ts[0]
         MarsWait = ts[1]
         MEDeltaT = ts[2]
 
-        # Set Times (Earth-Mars)
-        gmat.GetObject('Pathfinder').SetField("Epoch", str(float(EarthWait)))
-        gmat.GetObject('EMDeltaT').SetField("Value", EMDeltaT)
+        # Convert Transfer Times to [s]
+        EMDeltaT_s = EMDeltaT * 86400
+        MEDeltaT_s = MEDeltaT * 86400
 
-        # Set Earth Departure Burn
-        gmat.GetObject('EarthDepartureBurn').SetField("Element1", EarthDepartureBurn[0])
-        gmat.GetObject('EarthDepartureBurn').SetField("Element2", EarthDepartureBurn[1])
-        gmat.GetObject('EarthDepartureBurn').SetField("Element3", EarthDepartureBurn[2])
+        # Find the dates of the burns
+        t_EDB = EarthDepart
+        t_MAB = t_EDB + EMDeltaT
+        t_MDB = t_MAB + MarsWait
+        t_EAB = t_MDB + MEDeltaT
 
-        # Set Mars Arrival Burn
-        gmat.GetObject('MarsArrivalBurn').SetField("Element1", MarsArrivalBurn[0])
-        gmat.GetObject('MarsArrivalBurn').SetField("Element2", MarsArrivalBurn[1])
-        gmat.GetObject('MarsArrivalBurn').SetField("Element3", MarsArrivalBurn[2])
-
-        # Run GMAT
-        gmat.RunScript()
-   
-        # Get Vars (Earth to Mars)
-        MarsRMAG = gmat.GetRuntimeObject("MarsRMAG").GetField("Value")
-        MarsVX = gmat.GetRuntimeObject("MarsVX").GetField("Value")
-        MarsVY = gmat.GetRuntimeObject("MarsVY").GetField("Value")
-        MarsVZ = gmat.GetRuntimeObject("MarsVZ").GetField("Value")
+        # Find Planet Positions and Velocities for transfer
+        def get_state(body, jd):
+            with solar_system_ephemeris.set('jpl'):
+                pos, vel = get_body_barycentric_posvel(body, Time(jd, format='jd'))
+            return pos.xyz.to_value('km').flatten(), vel.xyz.to_value('km/s').flatten()
         
-        ### Mars-Earth ###
-        gmat.LoadScript('C:\\Users\\18475\\Desktop\\Projects\\GMAT\\missions\\Pathfinder_ME.script')
+        R_E1, V_E1 = get_state('earth', t_EDB)
+        R_M1, V_M1 = get_state('mars', t_MAB)
+        R_M2, V_M2 = get_state('mars', t_MDB)
+        R_E2, V_E2 = get_state('earth', t_EAB)
 
-        Mars_Departure = EarthWait + EMDeltaT + MarsWait
+        # Solve Lambert's Problem
+        v1_E, v2_M, _ = UV_Lambert_Bisect(R_E1, R_M1, mu_sun, EMDeltaT_s)
+        v1_M, v2_E, _ = UV_Lambert_Bisect(R_M2, R_E2, mu_sun, MEDeltaT_s)
+    
+        # Compute Burns
+        EDB = v1_E - V_E1
+        MAB = V_M1 - v2_M
+        MDB = v1_M - V_M2
+        EAB = V_E2 - v2_E
 
-        # Set Times
-        gmat.GetObject('Pathfinder').SetField("Epoch", str(float(Mars_Departure)))
-        gmat.GetObject('MEDeltaT').SetField("Value", MEDeltaT)
+        # Calculate the sum of the delta_v magnitudes
+        delta_v = np.linalg.norm(EDB) + np.linalg.norm(MAB) + np.linalg.norm(MDB) + np.linalg.norm(EAB)
 
-        # Set Mars Departure Burn
-        gmat.GetObject('MarsDepartureBurn').SetField("Element1", MarsDepartureBurn[0])
-        gmat.GetObject('MarsDepartureBurn').SetField("Element2", MarsDepartureBurn[1])
-        gmat.GetObject('MarsDepartureBurn').SetField("Element3", MarsDepartureBurn[2])
-
-        # Set Mars Arrival Burn
-        gmat.GetObject('EarthArrivalBurn').SetField("Element1", EarthArrivalBurn[0])
-        gmat.GetObject('EarthArrivalBurn').SetField("Element2", EarthArrivalBurn[1])
-        gmat.GetObject('EarthArrivalBurn').SetField("Element3", EarthArrivalBurn[2])
-
-        # Run GMAT
-        gmat.RunScript()
-
-        # Get Vars (Earth to Mars)
-        EarthRMAG = gmat.GetRuntimeObject("EarthRMAG").GetField("Value")
-        EarthVX = gmat.GetRuntimeObject("EarthVX").GetField("Value")
-        EarthVY = gmat.GetRuntimeObject("EarthVY").GetField("Value")
-        EarthVZ = gmat.GetRuntimeObject("EarthVZ").GetField("Value")
-
-        # Calculate delta-v
-        delta_v = np.linalg.norm(EarthDepartureBurn) + np.linalg.norm(MarsArrivalBurn) + np.linalg.norm(MarsDepartureBurn) + np.linalg.norm(EarthArrivalBurn)
-
-        # Outputs
         outputs['delta_v'] = delta_v
-        outputs['MarsRMAG'] = MarsRMAG
-        outputs['MarsVX'] = MarsVX
-        outputs['MarsVY'] = MarsVY
-        outputs['MarsVZ'] = MarsVZ
-        outputs['EarthRMAG'] = EarthRMAG
-        outputs['EarthVX'] = EarthVX
-        outputs['EarthVY'] = EarthVY
-        outputs['EarthVZ'] = EarthVZ
+        outputs['EDB'] = EDB
+        outputs['MAB'] = MAB
+        outputs['MDB'] = MDB
+        outputs['EAB'] = EAB
 
 #Fuel Burn Block
 class Fuel_Burn(om.ExplicitComponent):
@@ -249,54 +268,27 @@ prob.model = ModelGroup()
 #Set the optimizer
 prob.driver = om.ScipyOptimizeDriver()
 prob.driver.options["optimizer"] = "SLSQP"
-prob.driver.options["maxiter"] = 100
+prob.driver.options["maxiter"] = 1000
 prob.driver.options["tol"] = 1e-6
 prob.driver.options["debug_print"] = ["nl_cons", "objs", "desvars"]
 # prob.driver.options["print_opt_prob"] = True
 
 #set the grobal input
 prob.model.set_input_defaults("m_s",val=20000.0) #due to initial value is sensitive  
-prob.model.set_input_defaults("ts",val=np.array([208, 500, 270]))
-prob.model.set_input_defaults("t_start",val=608.0)
+prob.model.set_input_defaults("ts",val=np.array([272.42149202, 422.89488847, 252.57764301]))
+prob.model.set_input_defaults("t_start",val=571.0)
 
-# generate Initial guess 
-# Initial Guess from last Run
-ts_inital = np.array([248.55026636, 496.87138656, 269.94184923])
-t_start_initial = 603.24347764
-EarthDepartureBurn_Initial = np.array([-1.53354101,  4.81383612,  0.21110436])
-MarsArrivalBurn_Initial = np.array([-3.82042382,  0.01523331,  0.06887896])
-MarsDepatureBurn_Initial = np.array([ 3.76004347,  0.23218221, -1.3525757 ])
-EarthArrivalBurn_Initial = np.array([ 1.45271153, -6.6920759 , -7.55663744])
-
-prob.model.set_input_defaults("ts",val=ts_inital)
-prob.model.set_input_defaults("t_start",val=t_start_initial)
-prob.model.set_input_defaults("EDB", val=EarthDepartureBurn_Initial)
-prob.model.set_input_defaults("MAB", val=MarsArrivalBurn_Initial)
-prob.model.set_input_defaults("MDB", val=MarsDepatureBurn_Initial)
-prob.model.set_input_defaults("MAB", val=EarthArrivalBurn_Initial)
 
 #set the design varaibles, objectives, constrints
 prob.model.add_design_var("m_s", lower=0, upper=200000)
 #prob.model.add_design_var("m_d", lower=0, upper=200000)
-prob.model.add_design_var("ts", lower=0, upper=2*365)
+prob.model.add_design_var("ts", lower=200, upper=2*365)
 prob.model.add_design_var("t_start", lower=0, upper=3650)
-prob.model.add_design_var("EDB")
-prob.model.add_design_var("MAB")
-prob.model.add_design_var("MDB")
-prob.model.add_design_var("EAB")
 
 prob.model.add_objective("Df")
 prob.model.add_constraint("m_s", lower=0, upper=40000)
-prob.model.add_constraint("F", lower=0, upper=98000000)
-prob.model.add_constraint('MarsRMAG', upper=13000, lower=11000)
-prob.model.add_constraint("MarsVX", upper=0.1, lower=-0.1)
-prob.model.add_constraint("MarsVY", upper=0.1, lower=-0.1)
-prob.model.add_constraint("MarsVZ", upper=0.1, lower=-0.1)
-prob.model.add_constraint('EarthRMAG', upper=13000, lower=11000)
-prob.model.add_constraint("EarthVX", upper=0.1, lower=-0.1)
-prob.model.add_constraint("EarthVY", upper=0.1, lower=-0.1)
-prob.model.add_constraint("EarthVZ", upper=0.1, lower=-0.1)
-#Add other constraints if necessary 
+prob.model.add_constraint("F", lower=0, upper=7000000)
+
 
 # Ask OpenMDAO to finite-difference across the model to compute the gradients for the optimizer
 prob.model.approx_totals()
@@ -308,11 +300,12 @@ prob.set_solver_print(level=0)
 prob.run_driver()
 
 print("minimum found at")
-print(prob.get_val("m_s"))
-print(prob.get_val("m_d"))
-print(prob.get_val("ts"))
-print(prob.get_val("delta_v"))
+print("Shelid Mass :", prob.get_val("m_s"))
+print("Dry Mass :", prob.get_val("m_d"))
+print("Times :", prob.get_val("ts"))
+print("Total delta-v :", prob.get_val("delta_v"))
+print("Fuel Burn", prob.get_val("F"))
 
 print("minumum objective")
-print(prob.get_val("F"))
+print(prob.get_val("Df"))
 
